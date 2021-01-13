c77d3ec9d6a16c8f7a23bf9e8e3015429458ea32
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2021-01-13 15:09:45 -0300

    fix: dev landing index.js styles moved to styles.js

--- src/pages/DevLanding/index.js
+++ src/pages/DevLanding/index.js
@@ -5,7 +5,7 @@ import { useNavigation } from '@react-navigation/native'
 
 import { useAuth } from '../../context/auth'
 
-import { DARK_GRAY_COLOR, MAIN_COLOR, RED_COLOR, WHITE_COLOR } from '../../../styles.global'
+import styles from './styles'
 
 function DevLanding() {
     const navigation = useNavigation()
@@ -20,42 +20,21 @@ function DevLanding() {
     }
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', paddingHorizontal: 20, backgroundColor: MAIN_COLOR }}>
-            <Text style={{ marginBottom: 50, fontSize: 23, fontFamily: 'Poppins_700Bold', color: WHITE_COLOR, textAlign: 'center' }}>Painel do Administrador</Text>
+        <View style={styles.container}>
+            <Text style={styles.pageTitle}>Painel do Administrador</Text>
             <RectButton
                 onPress={handleGoToRegisterLecture}
-                style={{
-                    width: '100%',
-                    backgroundColor: WHITE_COLOR,
-                    borderRadius: 12,
-                    padding: 7,
-                    marginBottom: 10,
-                    alignItems: 'center'
-                }}
+                style={styles.registerLectureButton}
                 rippleColor="rgba(0, 0, 0, 0.2)"
             >
-                <Text style={{
-                    fontSize: 18,
-                    fontFamily: 'Poppins_600SemiBold',
-                    color: DARK_GRAY_COLOR
-                }}>Cadastrar aula</Text>
+                <Text style={styles.registerLectureButtonText}>Cadastrar aula</Text>
             </RectButton>
             <RectButton
                 onPress={handleLogout}
-                style={{
-                    width: '100%',
-                    backgroundColor: RED_COLOR,
-                    borderRadius: 12,
-                    padding: 7,
-                    alignItems: 'center'
-                }}
+                style={styles.logoutButton}
                 rippleColor="rgba(0, 0, 0, 0.2)"
             >
-                <Text style={{
-                    fontSize: 18,
-                    fontFamily: 'Poppins_600SemiBold',
-                    color: WHITE_COLOR
-                }}>Logout</Text>
+                <Text style={styles.logoutButtonText}>Logout</Text>
             </RectButton>
         </View>
     )
---
+++ src/pages/DevLanding/styles.js
@@ -0,0 +1,51 @@
+import { StyleSheet } from 'react-native'
+
+import { DARK_GRAY_COLOR, MAIN_COLOR, RED_COLOR, WHITE_COLOR } from '../../../styles.global'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 20,
+        backgroundColor: MAIN_COLOR
+    },
+
+    pageTitle: {
+        marginBottom: 50,
+        fontSize: 23,
+        fontFamily: 'Poppins_700Bold',
+        color: WHITE_COLOR,
+        textAlign: 'center'
+    },
+
+    registerLectureButton: {
+        width: '100%',
+        backgroundColor: WHITE_COLOR,
+        borderRadius: 12,
+        padding: 7,
+        marginBottom: 10,
+        alignItems: 'center'
+    },
+
+    registerLectureButtonText: {
+        fontSize: 18,
+        fontFamily: 'Poppins_600SemiBold',
+        color: DARK_GRAY_COLOR
+    },
+
+    logoutButton: {
+        width: '100%',
+        backgroundColor: RED_COLOR,
+        borderRadius: 12,
+        padding: 7,
+        alignItems: 'center'
+    },
+
+    logoutButtonText: {
+        fontSize: 18,
+        fontFamily: 'Poppins_600SemiBold',
+        color: WHITE_COLOR
+    }
+})
+
+export default styles
