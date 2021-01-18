edfbc11ec8e3dca0ce9490f5016a90d3c52a1144
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2021-01-18 11:59:09 -0300

    fix: app bottom tabs stylized

--- src/routes/homeTabs.routes.js
+++ src/routes/homeTabs.routes.js
@@ -8,17 +8,28 @@ import Options from '../pages/Options'
 
 import { useAuth } from '../context/auth'
 
+import { SimpleLineIcons, Feather, MaterialIcons } from '@expo/vector-icons'
+import { DIVISION_COLOR, MAIN_COLOR } from '../../styles.global'
+
 const AppTabs = createBottomTabNavigator()
 
 function HomeTabs() {
     const { user } = useAuth()
 
     return (
-        <AppTabs.Navigator>
-            <AppTabs.Screen name="Profile" component={Profile} initialParams={{ userId: user.uid }} />
-            <AppTabs.Screen name="Lectures" component={Lectures} />
-            <AppTabs.Screen name="Leaderboard" component={Leaderboard} />
-            <AppTabs.Screen name="Options" component={Options} />
+        <AppTabs.Navigator tabBarOptions={{ activeTintColor: MAIN_COLOR, inactiveTintColor: 'rgba(0, 0, 0, 0.5)', activeBackgroundColor: DIVISION_COLOR, showLabel: false }}>
+            <AppTabs.Screen name="Profile" component={Profile} initialParams={{ userId: user.uid }} options={{ tabBarIcon: ({ color }) => (
+                <Feather name="user" size={20} color={color} />
+            ) }} />
+            <AppTabs.Screen name="Lectures" component={Lectures} options={{ tabBarIcon: ({ color }) => (
+                <Feather name="book-open" size={20} color={color} />
+            ) }} />
+            <AppTabs.Screen name="Leaderboard" component={Leaderboard} options={{ tabBarIcon: ({ color }) => (
+                <MaterialIcons name="leaderboard" size={20} color={color} />
+            ) }} />
+            <AppTabs.Screen name="Options" component={Options} options={{ tabBarIcon: ({ color }) => (
+                <SimpleLineIcons name="options" size={20} color={color} />
+            ) }} />
         </AppTabs.Navigator>
     )
 }
